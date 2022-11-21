0b979fa262f3843d9a1d18e5c38e2695602f26c4
Nickolay <arbuzov.n@f-case.ru>
2022-11-21 19:52:57 +0300

    feat

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -5,12 +5,12 @@ import { PostType, BlogType, UserInputType, CommentType} from '../types';
 const mongoURI = process.env.MONGO_URL || "";
 
 export const client = new MongoClient(mongoURI);
-export const blogCollection = client.db("ht_03").collection<BlogType>("blogs",);
-export const postCollection = client.db("ht_03").collection<PostType>("posts");
-export const userCollection = client.db("ht_03").collection<UserInputType>("users");
-export const commentCollection = client.db("ht_03").collection<CommentType>("comments");
-export const jwtCollection = client.db("ht_03").collection("jwt");
-export const logCollection = client.db("ht_03").collection("logs");
+export const blogCollection = client.db("ht_08").collection<BlogType>("blogs",);
+export const postCollection = client.db("ht_08").collection<PostType>("posts");
+export const userCollection = client.db("ht_08").collection<UserInputType>("users");
+export const commentCollection = client.db("ht_08").collection<CommentType>("comments");
+export const jwtCollection = client.db("ht_08").collection("jwt");
+export const logCollection = client.db("ht_08").collection("logs");
 
 export async function runDb(){
     try {
